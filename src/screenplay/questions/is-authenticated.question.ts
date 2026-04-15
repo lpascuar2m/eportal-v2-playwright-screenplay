dbc9ff6bad89fba30b670eebb6c4acd565cbd8e1
Lenard Pascua <lpascua.r2m@gmail.com>
2026-04-15 16:02:28 +0800

    Fix auth check timing sensitivity in IsAuthenticated

--- src/screenplay/questions/is-authenticated.question.ts
+++ src/screenplay/questions/is-authenticated.question.ts
@@ -2,6 +2,8 @@ import { Actor, Question } from '@testla/screenplay-playwright';
 import { BrowseTheWeb } from '@testla/screenplay-playwright/web';
 import { DashboardScreen } from '../screens/dashboard.screen';
 
+const AUTHENTICATION_CHECK_TIMEOUT_MS = 5_000;
+
 export class IsAuthenticated extends Question<boolean> {
     private constructor() {
         super();
@@ -17,6 +19,15 @@ export class IsAuthenticated extends Question<boolean> {
             { evaluateVisible: false }
         );
 
-        return await locator.isVisible();
+        try {
+            await locator.waitFor({
+                state: 'visible',
+                timeout: AUTHENTICATION_CHECK_TIMEOUT_MS,
+            });
+
+            return true;
+        } catch {
+            return false;
+        }
     }
 }
